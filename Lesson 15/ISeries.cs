bf6ab9fab4520dc9c64ba4a62d227790f22dbf37
yxa.kravchenko@yandex.ru <yxa.kravchenko@yandex.ru>
2022-11-02 22:54:51 +0300

    Добавьте файлы проекта.

---
+++ Lesson 15/ISeries.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson_15
+{
+    interface ISeries
+    {
+        void SetStart(int x);
+        int GetNext();
+        void Reset();
+    }
+    class ArithProgression : ISeries
+    {
+        int step;// Объявляем переменную шаг и здесь храним его
+        int startValue;//Объявляем стартовое значение и здесь храним его
+        int currentValue;//Объявляем текущее значение и здесь храним его
+        public int GetNext()// Возвращает следующее число ряда
+        {
+            currentValue += step;
+            return currentValue;
+        }
+
+        public void Reset()//Сброс до начального значения
+        {
+            currentValue = startValue;
+        }
+
+        public void SetStart(int x)// Устанавливает начальное значение
+        {
+            startValue = x;
+            currentValue = startValue;
+        }
+        public void SetStep(int s)// Устанавливает шаг
+        {
+            step = s;
+        }
+    }
+        class GeomProgression : ISeries
+        {
+            int step;
+            int startValue;
+            int currentValue;
+            public int GetNext()
+            {
+                currentValue *= step;
+                return currentValue;
+            }
+
+            public void Reset()
+            {
+                currentValue = startValue;
+            }
+
+            public void SetStart(int x)
+            {
+                startValue = x;
+                currentValue = startValue;
+            }
+            public void SetStep(int s)
+            {
+                step = s;
+            }
+        }
+    }
+
+   
+
---
+++ Lesson 15/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson_15
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Арифметическая прогрессия
+            //ArithProgression arith = new ArithProgression();
+            //arith.SetStart (10);
+            //arith.SetStep(2);
+            //Console.WriteLine(arith.GetNext());
+            //Console.WriteLine(arith.GetNext());
+            //Console.WriteLine(arith.GetNext());
+            //arith.Reset();
+            //Console.WriteLine(arith.GetNext());
+            //Console.WriteLine(arith.GetNext());
+            //Console.ReadKey();  
+
+            //Геометрическая прогрессия
+            GeomProgression geom = new GeomProgression(); //Создаем экземпляр класса
+            geom.SetStart(15);// Устанавливаем начальное значение
+            geom.SetStep(2);// Устанавливаем шаг
+            Console.WriteLine(geom.GetNext());// Выводим значение
+            Console.WriteLine(geom.GetNext());
+            Console.WriteLine(geom.GetNext());
+            Console.WriteLine(geom.GetNext());
+            geom.Reset();// Сброс до начального значения
+            Console.WriteLine(geom.GetNext());// Выводим значение
+            Console.WriteLine(geom.GetNext());
+            Console.WriteLine(geom.GetNext());
+            Console.WriteLine(geom.GetNext());
+            Console.ReadKey();
+
+        }
+    }
+}
